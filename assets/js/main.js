457dfd16223700741ef60744208050ced6c05fbf
Yuriy <yu.arhipenko@gmail.com>
2022-10-19 09:52:47 +0300

    додав меню hamburger

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,6 @@
-
-$(document).ready(function () {
-
-  $('.header-navbar__hamburger').on('click', function () {
-    $(this).toggleClass('active');
-    $('.header-navbar__list').toggleClass('active');
-  });
-
+ $(document).ready(function(){
+	$('.header-navbar__btn').on('click',function(){
+		$(this).toggleClass('header-navbar__btn--active');
+    $('.header-navbar__list').stop(true, true).slideToggle(500);
+	});
 });
