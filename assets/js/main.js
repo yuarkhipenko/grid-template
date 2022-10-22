ed85367fc4a519c4f9e38686b5942b1333416bc9
Yuriy <yu.arhipenko@gmail.com>
2022-10-22 22:39:11 +0300

    виправлення navbar та form

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
- $(document).ready(function(){
-	$('.header-navbar__btn').on('click',function(){
+ $(document).ready(function () {
+	$('.header-navbar__btn').on('click', function () {
 		$(this).toggleClass('header-navbar__btn--active');
     $('.header-navbar__list').stop(true, true).slideToggle(500);
 	});
